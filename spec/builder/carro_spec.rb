0b5c7816035e64dd6a3132dfddf53e1c716e286f
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-02-14 19:32:13 -0200

    [builder] reduz largura do código de testes

--- spec/builder/carro_spec.rb
+++ spec/builder/carro_spec.rb
@@ -5,37 +5,37 @@ describe Carro do
       ano_modelo = 1999
       carro_invalido = Carro.new('modelo A', 'fabricante A', ano_fabricacao,
                                  'ABC1234', nil, nil, ano_modelo, nil, nil)
-      mensagem_de_erro = 'ano do modelo nao pode ser anterior ao ano de fabricacao'
+      erro = 'ano do modelo nao pode ser anterior ao ano de fabricacao'
       expect(carro_invalido.validar!).to eq(false)
       expect(carro_invalido.erros.size).to eq(1)
-      expect(carro_invalido.erros.first).to eq(mensagem_de_erro)
+      expect(carro_invalido.erros.first).to eq(erro)
     end
 
     it 'deve ter uma placa especificada' do
       carro_invalido = Carro.new('modelo A', 'fabricante A', nil,
                                  nil, nil, nil, nil, nil, nil)
-      mensagem_de_erro = 'placa não pode ser nulo'
+      erro = 'placa não pode ser nulo'
       expect(carro_invalido.validar!).to eq(false)
       expect(carro_invalido.erros.size).to eq(1)
-      expect(carro_invalido.erros.first).to eq(mensagem_de_erro)
+      expect(carro_invalido.erros.first).to eq(erro)
     end
 
     it 'deve ter um modelo especificado' do
       carro_invalido = Carro.new(nil, 'fabricante A', nil,
                                  'ABC1234', nil, nil, nil, nil, nil)
-      mensagem_de_erro = 'modelo não pode ser nulo'
+      erro = 'modelo não pode ser nulo'
       expect(carro_invalido.validar!).to eq(false)
       expect(carro_invalido.erros.size).to eq(1)
-      expect(carro_invalido.erros.first).to eq(mensagem_de_erro)
+      expect(carro_invalido.erros.first).to eq(erro)
     end
 
     it 'deve ter um fabricante especificado' do
       carro_invalido = Carro.new('modelo A',nil, nil,
                                  'ABC1234', nil, nil, nil, nil, nil)
-      mensagem_de_erro = 'fabricante não pode ser nulo'
+      erro = 'fabricante não pode ser nulo'
       expect(carro_invalido.validar!).to eq(false)
       expect(carro_invalido.erros.size).to eq(1)
-      expect(carro_invalido.erros.first).to eq(mensagem_de_erro)
+      expect(carro_invalido.erros.first).to eq(erro)
     end
   end
 end
