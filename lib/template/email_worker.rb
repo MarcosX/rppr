217c2be6d3311cb9406b52e7d06f62bade1fa115
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-03-28 18:59:52 -0300

    [template] refatora worker para utilizar instancia ao inves de classe

--- lib/template/email_worker.rb
+++ lib/template/email_worker.rb
@@ -2,11 +2,11 @@ require 'timeout'
 require_relative 'logger'
 
 class EmailWorker
-  @@limite_timeout = 10
-  @@limite_tentativas = 5
-  @@logger = Logger
+  def enviar parametros
+    @limite_timeout = 10
+    @limite_tentativas = 5
+    @logger = Logger
 
-  def self.enviar parametros
     usuario = buscar_usuario(parametros[:usuario])
     corpo = gerar_mensagem(parametros[:mensagem],
                            usuario)
@@ -14,7 +14,7 @@ class EmailWorker
                            usuario)
     contador_tentativas = 0
     begin
-      timeout(@@limite_timeout) do
+      timeout(@limite_timeout) do
         enviar_email(de: usuario,
                      para: parametros[:destinatarios],
                      assunto: assunto,
@@ -22,21 +22,21 @@ class EmailWorker
       end
     rescue Timeout::Error
       contador_tentativas += 1
-      @@logger.error("Timeout::Error em EmailWorker.enviar_email")
-      retry if contador_tentativas < @@limite_tentativas
+      @logger.error("Timeout::Error em EmailWorker.enviar_email")
+      retry if contador_tentativas < @limite_tentativas
     end
   end
 
   private
-  def self.gerar_mensagem(tipo_de_mensagem, usuario)
+  def gerar_mensagem(tipo_de_mensagem, usuario)
     "corpo do email de #{tipo_de_mensagem} enviado por #{usuario}"
   end
 
-  def self.gerar_assunto(tipo_de_mensagem, usuario)
+  def gerar_assunto(tipo_de_mensagem, usuario)
     "#{tipo_de_mensagem} enviado por #{usuario}"
   end
 
-  def self.enviar_email(parametros)
+  def enviar_email(parametros)
     {
       para: parametros[:para],
       assunto: parametros[:assunto],
@@ -44,7 +44,7 @@ class EmailWorker
     }
   end
 
-  def self.buscar_usuario(id_usuario)
+  def buscar_usuario(id_usuario)
     "Usuario #{id_usuario}"
   end
 end
--- spec/template/email_worker_spec.rb
+++ spec/template/email_worker_spec.rb
@@ -5,7 +5,7 @@ describe EmailWorker do
                               'outro_email@email.com',
                               'email_qualquer@email.com']
     assunto = 'convite enviado por Usuario 1'
-    email = EmailWorker.enviar(
+    email = EmailWorker.new.enviar(
               usuario: id_usuario,
               destinatarios: lista_de_destinatarios,
               mensagem: :convite)
@@ -15,35 +15,38 @@ describe EmailWorker do
   end
 
   it 'deve gerenciar erros de timeout' do
-    allow(EmailWorker).to receive(:enviar_email).and_raise(Timeout::Error)
+    worker = EmailWorker.new
+    allow(worker).to receive(:enviar_email).and_raise(Timeout::Error)
     id_usuario = 1
     lista_de_destinatarios = ['email@email.com']
     expect {
-      EmailWorker.enviar(
+      worker.enviar(
         usuario: id_usuario,
         destinatarios: lista_de_destinatarios,
         mensagem: :convite) }.not_to raise_error
   end
 
   it 'deve logar erros ao enviar email' do
-    allow(EmailWorker).to receive(:enviar_email).and_raise(Timeout::Error)
+    worker = EmailWorker.new
+    allow(worker).to receive(:enviar_email).and_raise(Timeout::Error)
     id_usuario = 1
     lista_de_destinatarios = ['email@email.com']
     log_esperado = 'Timeout::Error em EmailWorker.enviar_email'
     expect(Logger).to receive(:error).with(log_esperado).at_least(1).times
-    EmailWorker.enviar(
+    worker.enviar(
       usuario: id_usuario,
       destinatarios: lista_de_destinatarios,
       mensagem: :convite)
   end
 
   it 'deve enviar email mesmo apos 4 falhas' do
-    expect(EmailWorker).to receive(:enviar_email).exactly(4).times.and_raise(Timeout::Error)
-    expect(EmailWorker).to receive(:enviar_email).once
+    worker = EmailWorker.new
+    expect(worker).to receive(:enviar_email).exactly(4).times.and_raise(Timeout::Error)
+    expect(worker).to receive(:enviar_email).once
     id_usuario = 1
     lista_de_destinatarios = ['email@email.com']
     log_esperado = 'Timeout::Error em EmailWorker.enviar_email'
-    EmailWorker.enviar(
+    worker.enviar(
       usuario: id_usuario,
       destinatarios: lista_de_destinatarios,
       mensagem: :convite)
